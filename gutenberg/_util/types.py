dae793f00646991e6a2db5854be92841daec8cbf
Clemens Wolff <clemens.wolff+git@gmail.com>
2015-02-22 21:17:48 +0000

    Move types.py unit-tests to test directory

--- gutenberg/_util/types.py
+++ gutenberg/_util/types.py
@@ -10,28 +10,3 @@ def validate_etextno(etextno):
         msg = 'e-text identifiers should be strictly positive integers'
         raise ValueError(msg)
     return etextno
-
-
-if __name__ == '__main__':
-    # pylint: disable=C0111
-    # pylint: disable=R0904
-    import unittest
-
-    class Test(unittest.TestCase):
-        def test_is_valid_etext(self):
-            self.assertIsNotNone(validate_etextno(1))
-            self.assertIsNotNone(validate_etextno(12))
-            self.assertIsNotNone(validate_etextno(123))
-            self.assertIsNotNone(validate_etextno(1234))
-
-        def test_is_invalid_etext(self):
-            with self.assertRaises(ValueError):
-                validate_etextno('not-a-positive-integer')
-            with self.assertRaises(ValueError):
-                validate_etextno(-123)
-            with self.assertRaises(ValueError):
-                validate_etextno(0)
-            with self.assertRaises(ValueError):
-                validate_etextno(12.3)
-
-    unittest.main()
---
+++ tests/test_util.py
@@ -0,0 +1,25 @@
+# pylint: disable=C0111
+# pylint: disable=R0904
+
+
+import unittest
+
+from gutenberg._util.types import validate_etextno
+
+
+class TestUtilTypes(unittest.TestCase):
+    def test_is_valid_etext(self):
+        self.assertIsNotNone(validate_etextno(1))
+        self.assertIsNotNone(validate_etextno(12))
+        self.assertIsNotNone(validate_etextno(123))
+        self.assertIsNotNone(validate_etextno(1234))
+
+    def test_is_invalid_etext(self):
+        with self.assertRaises(ValueError):
+            validate_etextno('not-a-positive-integer')
+        with self.assertRaises(ValueError):
+            validate_etextno(-123)
+        with self.assertRaises(ValueError):
+            validate_etextno(0)
+        with self.assertRaises(ValueError):
+            validate_etextno(12.3)
