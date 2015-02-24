5446d53214683693fd2f55e0bc343f513bbf0c6f
Clemens Wolff <clemens.wolff+git@gmail.com>
2015-02-24 09:59:22 +0000

    Refactor: don't hardcode test metadata e-text ids

--- tests/util.py
+++ tests/util.py
@@ -27,44 +27,48 @@ def _mock_metadata_cache():
     with tempfile.NamedTemporaryFile(delete=False) as metadata_file:
         with gzip.GzipFile(fileobj=metadata_file, mode='wb') as gzip_file:
             gzip_file.write('\n'.join([
-                r'<http://www.gutenberg.org/ebooks/2701> '
+                r'<http://www.gutenberg.org/ebooks/{newstyle_etextno}> '
                 r'<http://www.w3.org/1999/02/22-rdf-syntax-ns#type> '
                 r'<http://www.gutenberg.org/2009/pgterms/ebook> '
                 r'.',
-                r'<http://www.gutenberg.org/ebooks/2701> '
+                r'<http://www.gutenberg.org/ebooks/{newstyle_etextno}> '
                 r'<http://purl.org/dc/terms/creator> '
                 r'<http://www.gutenberg.org/2009/agents/9> '
                 r'.',
-                r'<http://www.gutenberg.org/ebooks/2701> '
+                r'<http://www.gutenberg.org/ebooks/{newstyle_etextno}> '
                 r'<http://purl.org/dc/terms/title> '
                 r'"Moby Dick; Or, The Whale" '
 
                 r'.',
-                r'<http://www.gutenberg.org/ebooks/5> '
+                r'<http://www.gutenberg.org/ebooks/{oldstyle_etextno}> '
                 r'<http://www.w3.org/1999/02/22-rdf-syntax-ns#type> '
                 r'<http://www.gutenberg.org/2009/pgterms/ebook> '
                 r'.',
-                r'<http://www.gutenberg.org/ebooks/5> '
+                r'<http://www.gutenberg.org/ebooks/{oldstyle_etextno}> '
                 r'<http://purl.org/dc/terms/creator> '
                 r'<http://www.gutenberg.org/2009/agents/1> '
                 r'.',
-                r'<http://www.gutenberg.org/ebooks/5> '
+                r'<http://www.gutenberg.org/ebooks/{oldstyle_etextno}> '
                 r'<http://purl.org/dc/terms/title> '
                 r'"The United States Constitution" '
                 r'.',
 
-                r'<http://www.gutenberg.org/ebooks/14287> '
+                r'<http://www.gutenberg.org/ebooks/{unicode_etextno}> '
                 r'<http://www.w3.org/1999/02/22-rdf-syntax-ns#type> '
                 r'<http://www.gutenberg.org/2009/pgterms/ebook> '
                 r'.',
-                r'<http://www.gutenberg.org/ebooks/14287> '
+                r'<http://www.gutenberg.org/ebooks/{unicode_etextno}> '
                 r'<http://purl.org/dc/terms/creator> '
                 r'<http://www.gutenberg.org/2009/agents/60> '
                 r'.',
-                r'<http://www.gutenberg.org/ebooks/14287> '
+                r'<http://www.gutenberg.org/ebooks/{unicode_etextno}> '
                 r'<http://purl.org/dc/terms/title> '
                 r'"L\'\u00EEle myst\u00E9rieuse" '
                 r'.',
-            ]))
+            ]).format(
+                newstyle_etextno=NEWSTYLE_ETEXTNO,
+                oldstyle_etextno=OLDSTYLE_ETEXTNO,
+                unicode_etextno=UNICODE_ETEXTNO,
+            ))
 
     return metadata_file.name
